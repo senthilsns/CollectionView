2a23ba86ba494910ec708186ad82ff3bf8f75dbb
Senthil kumar K <nightrider.cbe@gmail.com>
2018-12-11 08:53:51 +0530

    Commit

---
+++ CollectionView/ImageCell.swift
@@ -0,0 +1,16 @@
+//
+//  ImageCell.swift
+//  CollectionView
+//
+//  Created by SENTHILKUMAR on 11/12/18.
+//  Copyright © 2018 Personal. All rights reserved.
+//
+
+import UIKit
+
+class ImageCell: UICollectionViewCell {
+ 
+    @IBOutlet var sampleImageView: UIImageView!
+    @IBOutlet var sampleLabel: UILabel!
+    
+}
--- CollectionView/ViewController.swift
+++ CollectionView/ViewController.swift
@@ -10,11 +10,71 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var collectionView: UICollectionView!
+    var numberArray :[String] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        for i in 1...10 {
+            
+            numberArray.append(String(i))
+            
+        }
+        
     }
-
-
+    
+    
 }
 
+
+
+extension ViewController : UICollectionViewDataSource {
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ImageCell", for: indexPath) as! ImageCell
+        
+        cell.layer.borderColor = UIColor.red.cgColor
+        cell.layer.borderWidth = 1
+        cell.layer.cornerRadius = 2
+        
+        cell.sampleLabel.text = numberArray[indexPath.row]
+        cell.sampleImageView.image = UIImage(named: "rose")
+        
+        return cell
+    }
+    
+    
+    // change background color when user touches cell
+    func collectionView(_ collectionView: UICollectionView, didHighlightItemAt indexPath: IndexPath) {
+        let cell = collectionView.cellForItem(at: indexPath)
+        cell?.backgroundColor = UIColor.yellow
+    }
+    
+    // change background color back when user releases touch
+    func collectionView(_ collectionView: UICollectionView, didUnhighlightItemAt indexPath: IndexPath) {
+        let cell = collectionView.cellForItem(at: indexPath)
+        cell?.backgroundColor = UIColor.white
+    }
+    
+}
+
+extension ViewController : UICollectionViewDelegate {
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let alert = UIAlertController(title: "Hi", message: "User", preferredStyle: .alert)
+        let action = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alert.addAction(action)
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+}
